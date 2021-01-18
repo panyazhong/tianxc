207929156de210efc4094b8877c76574d09e521e
panyazhong <pyz1226149198@gmail.com>
2021-01-18 17:30:55 +0800

    新增获取模块使用信息接口

--- src/app/api/beaconAPI.ts
+++ src/app/api/beaconAPI.ts
@@ -4,6 +4,7 @@ import generatorRes from '../../utils/generatorRes';
 import { Code } from './config';
 import jwt from 'jsonwebtoken';
 import { tokenConfig } from '../../utils/config';
+import { ParamName } from 'koa-router';
 
 @controller('/api/behavior')
 class Role {
@@ -32,7 +33,81 @@ class Role {
       }
     } catch (error) {}
   }
+
+  @get('/getModulesInfo')
+  async getUserBehavior(ctx: any) {
+    try {
+      let { start_time, end_time } = ctx.request.query;
+
+      if (start_time && end_time) {
+        start_time = new Date(start_time).getTime();
+        end_time = new Date(end_time).getTime();
+      } else {
+        // 默认七天
+        let date = new Date();
+        let day = date.getDate();
+        start_time = new Date(date.setDate(day - 7)).getTime();
+        end_time = new Date().getTime();
+      }
+
+      const res = await behaviorModel
+        .find({
+          load_time: {
+            $gte: start_time,
+          },
+          leave_time: {
+            $lte: end_time,
+          },
+        })
+        .populate('user', {
+          username: 1,
+        });
+
+      // let users = <any>[];
+
+      // users = res.map((item: any) => item.user._id);
+
+      // users = Array.from(new Set(users));
+
+      interface resultInterface {
+        [paramName: string]: any;
+      }
+      let result: resultInterface = {};
+      res.forEach((item: any) => {
+        // users.forEach((id: any) => {
+        // if (item.user._id === id) {
+        // if (result[id]) {
+        // result[id] = mergeBehavior(result[id], JSON.parse(item.behavior));
+        // } else {
+        // result[id] = JSON.parse(item.behavior);
+        // }
+        // }
+        result = mergeBehavior(result, JSON.parse(item.behavior));
+        // });
+      });
+
+      ctx.response.body = generatorRes(Code.success, '', result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
+
+let mergeBehavior = (origin: any, behavior: any) => {
+  try {
+    for (let key in behavior) {
+      if (origin[key]) {
+        origin[key] += behavior[key];
+      } else {
+        origin[key] = behavior[key];
+      }
+    }
+
+    return origin;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 let checkToken = (authorization: string, ctx: any) => {
   if (!authorization) {
